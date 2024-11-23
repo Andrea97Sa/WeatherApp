a5d54a80af1161a58d17405ad265abce4154d16f
Andrea Sandomenico <andreasandomenico97@gmail.com>
2024-11-23 10:01:58 +0100

    Initial Commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ulixe on 23/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
